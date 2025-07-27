4ddd65043f15f8c40aa26d543e16173839c5e635
Yashas Shetty <yashasmshetty@Yashass-MacBook-Air.local>
2025-07-27 20:11:10 +0530

    added a scroll progress

--- src/components/magicui/scroll-progress.tsx
+++ src/components/magicui/scroll-progress.tsx
@@ -3,12 +3,9 @@
 import { cn } from "@/lib/utils";
 import { motion, MotionProps, useScroll } from "motion/react";
 import React from "react";
-interface ScrollProgressProps
-  extends Omit<React.HTMLAttributes<HTMLElement>, keyof MotionProps> {}
-
 export const ScrollProgress = React.forwardRef<
   HTMLDivElement,
-  ScrollProgressProps
+  Omit<React.HTMLAttributes<HTMLElement>, keyof MotionProps>
 >(({ className, ...props }, ref) => {
   const { scrollYProgress } = useScroll();
 
